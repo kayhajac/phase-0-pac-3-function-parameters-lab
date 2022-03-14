037af428aaa3880884965f65c4568495336b688b
Jack Kayhart <jackkayhart@gmail.com>
2022-03-13 22:30:58 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+let name = Aki 
+
+function introduction(name){
+    return "Hi, my name is ${name}."
+}
+
+function introductionWithLanguage(name, language) {
+    console.log(`The two values are ${name} and ${language}.`);
+    return "Hi, my name is $[name] and I am learning to program in $[language]."
+  }
+  
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    console.log(`The two values are ${name} and ${language}.`);
+    return "Hi, my name is $[name] and I am learning to program in $[language]."
+  }
